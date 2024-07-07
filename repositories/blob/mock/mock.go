8e3068dc159b340fd6bb96341f48d2aff1c2b5e1
Igor Shishkin <me@teran.dev>
2024-07-07 11:05:45 +0300

    Add BLOB repository mock
    Signed-off-by: Igor Shishkin <me@teran.dev>

---
+++ repositories/blob/mock/mock.go
@@ -0,0 +1,35 @@
+package mock
+
+import (
+	"context"
+	"io"
+
+	"github.com/stretchr/testify/mock"
+
+	"github.com/teran/archived/repositories/blob"
+)
+
+var _ blob.Repository = (*Mock)(nil)
+
+type Mock struct {
+	mock.Mock
+}
+
+func New() *Mock {
+	return &Mock{}
+}
+
+func (m *Mock) PutBlob(_ context.Context, key string, rd io.Reader) error {
+	data, err := io.ReadAll(rd)
+	if err != nil {
+		return err
+	}
+
+	args := m.Called(key, data)
+	return args.Error(0)
+}
+
+func (m *Mock) GetBlobURL(_ context.Context, key string) (string, error) {
+	args := m.Called(key)
+	return args.String(0), args.Error(1)
+}
