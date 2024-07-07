6fca4f80dce8b601313926bd583ff5d63ae8dd67
Igor Shishkin <me@teran.dev>
2024-07-07 18:39:24 +0300

    Add HTML presenter for access component
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.5
 require (
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.9.0
@@ -30,6 +31,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -40,8 +42,11 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/sys/mountinfo v0.7.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -51,6 +56,8 @@ require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/teran/go-random v0.0.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
 	go.opentelemetry.io/otel v1.26.0 // indirect
 	go.opentelemetry.io/otel/metric v1.26.0 // indirect
@@ -59,8 +66,10 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0 // indirect
 	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ presenter/access/html/handlers_test.go
@@ -0,0 +1,100 @@
+package html
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/stretchr/testify/suite"
+	"github.com/teran/archived/service"
+)
+
+func (s *handlersTestSuite) TestContainerIndex() {
+	s.serviceMock.On("ListContainers").Return([]string{"test-container-1"}, nil).Once()
+
+	s.compareHTMLResponse(s.srv.URL, "testdata/index.html.sample")
+}
+
+func (s *handlersTestSuite) TestVersionIndex() {
+	s.serviceMock.On("ListVersions", "test-container-1").Return([]string{"20241011121314"}, nil).Once()
+
+	s.compareHTMLResponse(s.srv.URL+"/test-container-1/", "testdata/versions.html.sample")
+}
+
+func (s *handlersTestSuite) TestObjectIndex() {
+	s.serviceMock.On("ListObjects", "test-container-1", "20241011121314").Return([]string{"test-object-dir/file.txt"}, nil).Once()
+
+	s.compareHTMLResponse(s.srv.URL+"/test-container-1/20241011121314/", "testdata/objects.html.sample")
+}
+
+func (s *handlersTestSuite) TestGetObject() {
+	s.serviceMock.On("GetObjectURL", "test-container-1", "20241011121314", "test-dir/filename.txt").Return("https://example.com/some-addr", nil).Once()
+
+	req, err := http.NewRequest(http.MethodGet, s.srv.URL+"/test-container-1/20241011121314/test-dir/filename.txt", nil)
+	s.Require().NoError(err)
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := client.Do(req)
+	s.Require().NoError(err)
+
+	v := resp.Header.Get("Location")
+	s.Require().Equal("https://example.com/some-addr", v)
+}
+
+// Definitions ...
+type handlersTestSuite struct {
+	suite.Suite
+
+	srv *httptest.Server
+
+	serviceMock *service.Mock
+	handlers    Handlers
+}
+
+func (s *handlersTestSuite) SetupTest() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	s.serviceMock = service.NewMock()
+
+	s.handlers = New(s.serviceMock, "templates")
+	s.handlers.Register(e)
+
+	s.srv = httptest.NewServer(e)
+}
+
+func (s *handlersTestSuite) TearDownTest() {
+	s.serviceMock.AssertExpectations(s.T())
+
+	s.srv.Close()
+}
+
+func TestHandlersTestSuite(t *testing.T) {
+	suite.Run(t, &handlersTestSuite{})
+}
+
+func (s *handlersTestSuite) compareHTMLResponse(url, responseSamplePath string) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	s.Require().NoError(err)
+
+	resp, err := http.DefaultClient.Do(req)
+	s.Require().NoError(err)
+
+	sampleData, err := os.ReadFile(responseSamplePath)
+	s.Require().NoError(err)
+
+	data, err := io.ReadAll(resp.Body)
+	s.Require().NoError(err)
+
+	s.Require().Equal(sampleData, data)
+}
