c8b61b4beac896e1c1177f29103c85b36be8e02b
Igor Shishkin <me@teran.dev>
2024-07-09 22:04:04 +0300

    Add GetBlobKeyByObject method to metadata repository (#24)

--- repositories/metadata/postgresql/blobs.go
+++ repositories/metadata/postgresql/blobs.go
@@ -3,6 +3,7 @@ package postgresql
 import (
 	"context"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/pkg/errors"
 )
 
@@ -23,4 +24,28 @@ func (r *repository) CreateBLOB(ctx context.Context, checksum string, size uint6
 		ExecContext(ctx)
 
 	return errors.Wrap(err, "error executing SQL query")
+}
+
+func (r *repository) GetBlobKeyByObject(ctx context.Context, container, version, key string) (string, error) {
+	row := psql.
+		Select("b.checksum AS checksum").
+		From("blobs b").
+		Join("objects o ON o.blob_id = b.id").
+		Join("versions v ON o.version_id = v.id").
+		Join("containers c ON v.container_id = c.id").
+		Where(sq.Eq{
+			"c.name":         container,
+			"v.name":         version,
+			"o.key":          key,
+			"v.is_published": true,
+		}).
+		RunWith(r.db).
+		QueryRowContext(ctx)
+
+	var checksum string
+	if err := row.Scan(&checksum); err != nil {
+		return "", errors.Wrap(err, "error looking up BLOB")
+	}
+
+	return checksum, nil
 }
--- repositories/metadata/postgresql/blobs_test.go
+++ repositories/metadata/postgresql/blobs_test.go
@@ -1,6 +1,29 @@
 package postgresql
 
 func (s *postgreSQLRepositoryTestSuite) TestBlobs() {
-	err := s.repo.CreateBLOB(s.ctx, "deadbeef", 15, "text/plain")
+	const (
+		containerName = "test-container"
+		checksum      = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+	)
+
+	s.tp.On("Now").Return("2024-01-02T01:02:03Z").Once()
+
+	err := s.repo.CreateContainer(s.ctx, containerName)
 	s.Require().NoError(err)
+
+	versionID, err := s.repo.CreateVersion(s.ctx, containerName)
+	s.Require().NoError(err)
+
+	err = s.repo.CreateBLOB(s.ctx, checksum, 15, "text/plain")
+	s.Require().NoError(err)
+
+	err = s.repo.CreateObject(s.ctx, containerName, versionID, "test-object.txt", checksum)
+	s.Require().NoError(err)
+
+	err = s.repo.MarkVersionPublished(s.ctx, containerName, versionID)
+	s.Require().NoError(err)
+
+	casKey, err := s.repo.GetBlobKeyByObject(s.ctx, containerName, versionID, "test-object.txt")
+	s.Require().NoError(err)
+	s.Require().Equal(checksum, casKey)
 }
