8035778cfe9fef51299aee6facdd5670657addc6
Igor Shishkin <me@teran.dev>
2024-08-04 13:40:26 +0300

    Use configuration struct for gc service initialization (#93)
    Signed-off-by: Igor Shishkin <me@teran.dev>

---
+++ gc/service/config.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+
+	"github.com/teran/archived/repositories/metadata"
+)
+
+type Config struct {
+	MdRepo                   metadata.Repository
+	DryRun                   bool
+	UnpublishedVersionMaxAge time.Duration
+}
+
+func (c Config) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.MdRepo, validation.Required),
+		validation.Field(&c.DryRun),
+		validation.Field(&c.UnpublishedVersionMaxAge, validation.Required, validation.Min(time.Hour)),
+	)
+}
