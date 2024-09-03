7a6d84d3c3ff7fcb6dadaeaa2c16218271e44b38
Igor Shishkin <me@teran.dev>
2024-09-03 14:29:25 +0300

    Compact the code in the name of DRY!
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- appmetrics/appmetrics.go
+++ appmetrics/appmetrics.go
@@ -33,51 +33,15 @@ func New(livenessProbeFn, readinessProbeFn, startupProbeFn func() error) AppMetr
 }
 
 func (m *appMetrics) livenessProbe(c echo.Context) error {
-	if m.livenessProbeFn == nil {
-		return c.JSON(http.StatusNotImplemented, echo.Map{
-			"status": "failed", "error": "not implemented: check function is not provided",
-		})
-	}
-
-	if err := m.livenessProbeFn(); err != nil {
-		return c.JSON(http.StatusServiceUnavailable, echo.Map{
-			"status": "failed", "error": err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, echo.Map{"status": "ok"})
+	return check(c, m.livenessProbeFn)
 }
 
 func (m *appMetrics) readinessProbe(c echo.Context) error {
-	if m.readinessProbeFn == nil {
-		return c.JSON(http.StatusNotImplemented, echo.Map{
-			"status": "failed", "error": "not implemented: check function is not provided",
-		})
-	}
-
-	if err := m.readinessProbeFn(); err != nil {
-		return c.JSON(http.StatusServiceUnavailable, echo.Map{
-			"status": "failed", "error": err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, echo.Map{"status": "ok"})
+	return check(c, m.readinessProbeFn)
 }
 
 func (m *appMetrics) startupProbe(c echo.Context) error {
-	if m.startupProbeFn == nil {
-		return c.JSON(http.StatusNotImplemented, echo.Map{
-			"status": "failed", "error": "not implemented: check function is not provided",
-		})
-	}
-
-	if err := m.startupProbeFn(); err != nil {
-		return c.JSON(http.StatusServiceUnavailable, echo.Map{
-			"status": "failed", "error": err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, echo.Map{"status": "ok"})
+	return check(c, m.startupProbeFn)
 }
 
 func (m *appMetrics) metrics(c echo.Context) error {
@@ -89,4 +53,20 @@ func (m *appMetrics) Register(e *echo.Echo) {
 	e.GET(readinessProbeURL, m.readinessProbe)
 	e.GET(startupProbeURL, m.startupProbe)
 	e.GET(metricsURL, m.metrics)
+}
+
+func check(c echo.Context, fn func() error) error {
+	if fn == nil {
+		return c.JSON(http.StatusNotImplemented, echo.Map{
+			"status": "failed", "error": "not implemented: check function is not provided",
+		})
+	}
+
+	if err := fn(); err != nil {
+		return c.JSON(http.StatusServiceUnavailable, echo.Map{
+			"status": "failed", "error": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"status": "ok"})
 }
